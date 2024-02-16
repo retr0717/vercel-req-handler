294b8189cef8b6c2ea1e598a7b8631352647c2b6
retr0717 <alwinaji717@gmail.com>
2024-02-16 15:14:39 +0530

    handling build files from s3

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,48 @@
 const express = require('express');
-const {S3Client} = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 const dotenv = require('dotenv');
 dotenv.config();
+
+const s3Client = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY,
+    },
+});
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/*', async(req : any, res : any) => {
+app.get('/*', async (req: any, res: any) => {
 
-    //extracting the id from the subdomain of the website.
+    //Extracting the id from the subdomain of the website.
     const host = req.hostname;
     const id = host.split('.')[0];
+    const fpath = req.path;
+
+    // Define parameters for getting the object
+    const params = {
+        Bucket: "vercel",
+        Key: `dist/${id}${fpath}` //file path in the vercel
+    };
+
+    // Execute the command to get the object
+    try {
+        const contents = await s3Client.send(new GetObjectCommand(params));
+        console.log(contents); // This will contain the response from the GetObject API call
+
+        const type = fpath.endsWith("html") ? "text/html" : fpath.endsWith("css") ? "text/css" : "application/javascript"
+        res.set("Content-Type", type);
+
+        res.send(contents.Body);
+
+    } 
+    catch (error) 
+    {
+        console.error("Error getting object:", error);
+    }
 })
 
 app.listen(process.env.PORT, () => {
